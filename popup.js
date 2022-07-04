afbd8e113f6b654ea9d51ca0233753a04dc608b3
H0R15H0 <shohori13@gmail.com>
2022-07-04 18:52:15 +0900

    Add styles to result dialog.

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const DIALOG_OFFSET = 20
+const DIALOG_OFFSET = 5
 
 chrome.runtime.onMessage.addListener((request) => {
   switch (request.name)  {
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener((request) => {
 
       const resultDialog = buildResultDialog(...calculateCharacterCount(selection.toString()))
       resultDialog.style.position = 'absolute'
-      resultDialog.style.top = `${window.pageYOffset + selectionPosition.top + DIALOG_OFFSET}px`
+      resultDialog.style.top = `${window.pageYOffset + selectionPosition.bottom + DIALOG_OFFSET}px`
       resultDialog.style.left = `${selectionPosition.left}px`
 
       document.body.appendChild(resultDialog)
@@ -19,13 +19,7 @@ chrome.runtime.onMessage.addListener((request) => {
   }
 });
 
-function buildResultDialog(characterCounts, withoutExcludableCharacterCounts) {
-  const resultDialog = document.createElement('div')
-  resultDialog.id = "character-counter-result-dialog"
-  resultDialog.innerText = `Character Counter ${characterCounts} ${withoutExcludableCharacterCounts}`
-  return resultDialog
-}
-
+// calculation logics
 function calculateCharacterCount(string) {
   const segmenter = new Intl.Segmenter({ granularity: "grapheme" });
   const characters = [...segmenter.segment(string)] // each item looks like {segment: 'T', index: 0, input: 'This'}
@@ -42,4 +36,29 @@ function isExcludableCharacter(char) {
     default:
       return false
   }
+}
+
+// document manipulation
+function buildResultDialog(characterCounts, withoutExcludableCharacterCounts) {
+  const resultDialog = document.createElement('div')
+  resultDialog.id = "character-counter-result-dialog"
+  resultDialog.innerHTML = `
+    <div class="cc-box">
+      <p>CHARACTERS</p>
+      <p>${characterCounts}</p>
+    </div>
+    <div class="cc-box">
+      <p>CHARACTERS (without space)</p>
+      <p>${withoutExcludableCharacterCounts}</p>
+    </div>
+  `
+
+  document.addEventListener("click", (event) => {
+    const isClickInside = resultDialog.contains(event.target)
+    if (!isClickInside) {
+      resultDialog.remove()
+    }
+  })
+
+  return resultDialog
 }
